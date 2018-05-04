543b7bceab1962799f86291bd58153ca1a6a38bd
Alauddin Ahmed <37948276+itsaladin@users.noreply.github.com>
2018-05-04 21:00:34 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Games Animation</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="sky">
+		<img class="cloud" src="img/8.png">
+		<img class="cloud" src="img/8.png">
+		<img class="cloud" src="img/8.png">
+		<img class="cloud" src="img/8.png">
+	</div>
+	<div class="grass">
+		<img class="mario2" src="img/22.png">
+		<img class="mario2" src="img/33.png">
+	</div>
+	<div class="road">
+		<time class="time"></time>
+		<img class="mario" src="img/7.jpg">
+		<img class="mario2" src="img/mario.jpg">
+	</div>
+</body>
+</html>
