eaccca88c3e3218925324f0f9545a9368e81a65f
Alauddin Ahmed <37948276+itsaladin@users.noreply.github.com>
2018-07-05 12:10:59 +0600

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,7 @@
 		<img class="cloud" src="img/8.png">
 	</div>
 	<div class="grass">
-		<img class="mario2" src="img/22.png">
-		<img class="mario2" src="img/33.png">
+		
 	</div>
 	<div class="road">
 		<time class="time"></time>
